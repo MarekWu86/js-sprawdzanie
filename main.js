bdcf79d2b7b29d658d56f1b2536fa40627652e14
MarekWu86 <marek.wojdylo@m3group.pl>
2019-01-22 21:44:41 +0100

    sprawdzanie

---
+++ main.js
@@ -0,0 +1,11 @@
+var liczba = 35;
+
+function checkNumber(n) {
+    if (n >= 100 && n <= 200) {
+        console.log("Liczba znajduje się w przedziale"); 
+    } else {
+        console.log("Liczba nie znajduje się w przedziale");
+    }
+}
+
+checkNumber(liczba);
